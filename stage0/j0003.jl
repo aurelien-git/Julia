24bbb78316eb67ee9dafccf313d53ccb6c48cc5e
Aurélien Desbrières <aurelien@hackers.camp>
2016-03-06 06:24:54 +0100

    representation of min and max value of integers

---
+++ stage0/j0003.jl
@@ -0,0 +1,6 @@
+# Minimum and maximum representable value of primitive numeric type such as integers
+
+for T in [Int8,Int16,Int32,Int64,Int128,UInt8,UInt16,UInt32,UInt64,UInt128]
+    println("$(lpad(T,7)): [$(typemin(T)),$(typemax(T))]")
+end
+
