3681bf6bf61264d364b15cd421ac22e7689a63e7
Aurélien Desbrières <aurelien@hackers.camp>
2016-03-06 19:17:38 +0100

    division function to include directly in julia terminal

---
+++ stage0/j0008.jl
@@ -0,0 +1,12 @@
+#!/usr/bin/env julia
+# Using set precision
+
+function calcula(x, y)
+    x/y
+end
+
+#= use include("j0008.jl") from the julia> (itself launch from the same directory
+it will offer you the way to call the function directly as:
+calcula(5, 10)
+=#
+
