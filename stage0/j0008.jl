b830c6cf19c07db7af05641534301695bf771335
Aurélien Desbrières <aurelien@hackers.camp>
2016-03-06 19:29:00 +0100

    division function improved to float64 and BigFloat

--- stage0/j0008.jl
+++ stage0/j0008.jl
@@ -1,12 +1,17 @@
 #!/usr/bin/env julia
-# Using set precision
+# Include 2 function that can be call separatly from julia
 
 function calcula(x, y)
+    BigFloat(x/y)
+end
+
+function calculx(x, y)
     Float64(x/y)
 end
 
+
 #= use include("j0008.jl") from the julia> (itself launch from the same directory
 it will offer you the way to call the function directly as:
-calcula(5, 10)
+calcula(5, 10) or calculx(5, 10)
 =#
 
